855fd5c7c28f860fcba942426ee299e8cd93f2b2
Jelle Callewaert <jelle.callewaert@hotmail.com>
2021-01-03 23:15:07 +0100

    change privacy to contact

--- Beerhall/Controllers/HomeController.cs
+++ Beerhall/Controllers/HomeController.cs
@@ -23,7 +23,7 @@ namespace Beerhall.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Contact()
         {
             return View();
         }
